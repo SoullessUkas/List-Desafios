0da445e900e1115925f31a3ceea1943c0c1fa97e
Lucas Almeida <lucasmirandaalmeida2@gmail.com>
2024-09-26 14:14:09 -0300

    Organizando

---
+++ DesafioBootcamp/src/List/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,16 @@
+package List.OperacoesBasicas;
+
+public class Tarefa {
+//Atributo
+private String descricao;
+
+public Tarefa(String descricao) {
+    this.descricao = descricao;
+}
+
+public String getDescricao() {
+    return descricao;
+}
+
+
+}
