945ac4071bd95fdc9394a59a40a606f251ee58cc
Lucas Almeida <lucasmirandaalmeida2@gmail.com>
2024-09-27 09:17:59 -0300

    Ativadade de Ordenaçcao completa!

---
+++ DesafioBootcamp/src/List/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,50 @@
+package List.Ordenacao;
+//"Pessoa" como atributo. Cada pessoa possui atributos como nome, idade e altura. Implemente os seguintes métodos:
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+
+    private List<Pessoa> listaPessoas;
+
+    public OrdenacaoPessoas() {
+        this.listaPessoas = new ArrayList();
+    }
+
+    public void adicionarPessoa(String nome, double altura, int idade) {
+        listaPessoas.add(new Pessoa(nome, altura, idade));
+
+    }
+
+    public List<Pessoa> OrdenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(listaPessoas);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> OerdernarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(listaPessoas);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+        ordenacaoPessoas.adicionarPessoa("Alice", 1.65, 28);
+        ordenacaoPessoas.adicionarPessoa("Bruno", 1.80, 35);
+        ordenacaoPessoas.adicionarPessoa("Carla", 1.70, 22);
+        ordenacaoPessoas.adicionarPessoa("Daniel", 1.75, 30);
+        ordenacaoPessoas.adicionarPessoa("Eduarda", 1.60, 26);
+        ordenacaoPessoas.adicionarPessoa("Felipe", 1.85, 40);
+        ordenacaoPessoas.adicionarPessoa("Gabriela", 1.68, 33);
+        ordenacaoPessoas.adicionarPessoa("Hugo", 1.77, 29);
+        ordenacaoPessoas.adicionarPessoa("Isabela", 1.62, 25);
+        ordenacaoPessoas.adicionarPessoa("João", 1.90, 38);
+
+        System.out.println(" Por idade \n ------------- \n" + ordenacaoPessoas.OrdenarPorIdade());
+        System.out.println(" Por altura \n ------------- \n " + ordenacaoPessoas.OerdernarPorAltura());
+
+    }
+}
---
+++ DesafioBootcamp/src/List/Ordenacao/Pessoa.java
@@ -0,0 +1,50 @@
+package List.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Pessoa implements Comparable<Pessoa> {
+
+    private String nome;
+    private Double altura;
+    private int idade;
+
+    public Pessoa(String nome, Double altura, int idade) {
+        this.nome = nome;
+        this.altura = altura;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [nome=" + nome + ", altura=" + altura + ", idade=" + idade + "]\n";
+    }
+
+    @Override
+    public int compareTo(Pessoa p) {
+        return Integer.compare(idade, p.getIdade());
+    }
+
+    
+}
+class ComparatorPorAltura implements Comparator<Pessoa>{
+
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+       return Double.compare(p1.getAltura(), p2.getAltura());
+    }
+    
+}
